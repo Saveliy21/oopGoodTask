22e77a8eb40021477fb6c2b22bf3617824cde953
Saveliy21 <vildanov.s@bk.ru>
2023-04-17 21:51:23 +0500

    task-1 mini fix

--- src/main/java/ru/oop/Car.java
+++ src/main/java/ru/oop/Car.java
@@ -8,5 +8,7 @@ public class Car {
      * @param destination место назначения
      * @return ближашйие к destination место, до которого смогла доехать машина
      */
-    public Position drive(Position destination){}
+    public Position drive(Position destination){
+        return null;
+    }
 }
